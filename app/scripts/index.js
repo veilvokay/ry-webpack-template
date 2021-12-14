2742961db0a1fed0f2b1e5a88bc03d5ad6ce0417
Roman Yakovliev <romayakovlev@hotmail.com>
2021-12-15 00:06:20 +0200

    installed webpack-dev-server; added watch command; working with different types of files: 00:59:00

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,7 +1,10 @@
 
 import Post from "./Post";
+import data from '../mocks/data';
 import '../styles/index.css';
 
 const post = new Post('Webpack post');
 
 console.log('post ot string', post.toString());
+
+console.log('JSON: ', data);
