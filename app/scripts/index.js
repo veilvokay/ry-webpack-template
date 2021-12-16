ee82c98423713d82ae4f9d95ec30de7224a37135
Roman Yakovliev <romayakovlev@hotmail.com>
2021-12-16 19:08:13 +0200

    Fonts/xml-loader; CHUNKS !

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,11 +1,12 @@
-
-import Post from "./Post";
-import data from '../mocks/data';
-import WebpackLogo from '../assets/webpack-logo.png'
-import '../styles/index.css';
+import Post from './models/Post';
+// import data from '../mocks/data';
+// import xml from '../mocks/data.xml';
+import WebpackLogo from '../assets/webpack-logo.png';
+import 'styles/index.css';
 
 const post = new Post('Webpack post', WebpackLogo);
 
 console.log('post ot string', post.toString());
 
-console.log('JSON: ', data);
+// console.log('JSON: ', data);
+// console.log('XML: ', xml);
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
+const isProd = true;
+const getFilename = (n = '[name]') => isProd ? `${n}.[contenthash:6]` : n;
+
 module.exports = {
     context: path.resolve(__dirname, 'app'),
     mode: 'development',
@@ -12,6 +15,19 @@ module.exports = {
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.svg', '.png', '.js', '.jsx', '.ejs', '.json', '.html', '.sass', '.scss'],
+        alias: {
+            app: path.resolve(__dirname, './app/scripts/'),
+            assets: path.resolve(__dirname, './app/assets/'),
+            styles: path.resolve(__dirname, './app/styles/'),
+        }
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
     },
     plugins: [
         new HTMLWebpackPlugin({
@@ -28,6 +44,21 @@ module.exports = {
             {
                 test: /\.(png|jpg|svg|gif)$/,
                 type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: `assets/fonts/${getFilename()}[ext][query]`,
+                },
+            },
+            {
+                test: /\.xml$/,
+                // type: 'asset/resource',
+                use: ['xml-loader']
+                // generator: {
+                //     filename: 'mocks/[name][ext][query]',
+                // },
             }
         ]
     }
