a9e706fad3426290c19919f874aae0ceb409086f
Roman Yakovliev <romayakovlev@hotmail.com>
2021-12-16 18:30:13 +0200

    Assets Loader!!! use Asset Modules from webpack 5 instead of raw/url/file-loader

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,9 +1,10 @@
 
 import Post from "./Post";
 import data from '../mocks/data';
+import WebpackLogo from '../assets/webpack-logo.png'
 import '../styles/index.css';
 
-const post = new Post('Webpack post');
+const post = new Post('Webpack post', WebpackLogo);
 
 console.log('post ot string', post.toString());
 
